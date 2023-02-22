930dc67d7ed262855b3b5645a3899ce506a08ac1
kind sun dev <mr.duongtiendev@gmail.com>
2023-02-23 03:52:06 +0700

    set splash layout full sreen

--- app/src/main/java/com/kindsundev/expense/manager/view/splash/SplashingActivity.kt
+++ app/src/main/java/com/kindsundev/expense/manager/view/splash/SplashingActivity.kt
@@ -1,9 +1,15 @@
 package com.kindsundev.expense.manager.view.splash
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
 import android.widget.Toast
+import com.kindsundev.expense.manager.R
 import com.kindsundev.expense.manager.databinding.ActivitySplashingBinding
 import com.kindsundev.expense.manager.utils.isNetWorkAvailable
 import com.kindsundev.expense.manager.view.signin.SignInActivity
@@ -17,8 +23,21 @@ class SplashingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashingBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setLayoutFullScreen()
         checkNetworkStatus()
         waitAndNextActivity()
+    }
+
+    private fun setLayoutFullScreen() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
     }
 
     private fun waitAndNextActivity() {
