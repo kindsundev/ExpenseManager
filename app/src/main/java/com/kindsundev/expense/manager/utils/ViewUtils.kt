15c7b7f9d11bdbd5e50c74441305c882d0ed2800
kind sun dev <mr.duongtiendev@gmail.com>
2023-02-27 00:18:52 +0700

    create extends function start to signin

--- app/src/main/java/com/kindsundev/expense/manager/utils/ViewUtils.kt
+++ app/src/main/java/com/kindsundev/expense/manager/utils/ViewUtils.kt
@@ -2,10 +2,29 @@ package com.kindsundev.expense.manager.utils
 
 import android.content.Context
 import android.content.Intent
-import com.kindsundev.expense.manager.view.home.HomeActivity
+import androidx.fragment.app.FragmentManager
+import com.kindsundev.expense.manager.common.Constant
+import com.kindsundev.expense.manager.ui.custom.LoadingDialog
+import com.kindsundev.expense.manager.ui.home.HomeActivity
+import com.kindsundev.expense.manager.ui.signin.SignInActivity
+
+fun Context.startSignInActivity() =
+    Intent(this, SignInActivity::class.java).also {
+        it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(it)
+    }
 
 fun Context.startHomeActivity() =
     Intent(this, HomeActivity::class.java).also {
         it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(it)
     }
+
+fun startLoadingDialog(manager: FragmentManager, status: Boolean) {
+    val progress = LoadingDialog()
+    if (status) {
+        progress.show(manager, Constant.LOADING_DIALOG_NAME)
+    } else {
+        progress.dismiss()
+    }
+}
