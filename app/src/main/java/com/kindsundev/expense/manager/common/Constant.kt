e548486b59c4888b5b9c89929c809248262ad660
kind sun dev <mr.duongtiendev@gmail.com>
2023-02-27 00:15:03 +0700

    create a class that holds key constants

--- app/src/main/java/com/kindsundev/expense/manager/common/Constant.kt
+++ app/src/main/java/com/kindsundev/expense/manager/common/Constant.kt
@@ -1,4 +1,8 @@
 package com.kindsundev.expense.manager.common
 
 class Constant {
+    companion object {
+        const val LOADING_DIALOG_NAME = "LOADING_FRAGMENT"
+        const val LOGOUT_DIALOG_NAME = "LOGOUT_FRAGMENT"
+    }
 }
