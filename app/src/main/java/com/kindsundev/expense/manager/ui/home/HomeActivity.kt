f23e72104cded0b03ddef85b5e6e1a05687dfe48
kind sun dev <mr.duongtiendev@gmail.com>
2023-02-27 23:39:35 +0700

    check savedInstanceState in HomeActivity, add attribute popup between MenuFragment vs ProfileFragment

--- app/src/main/java/com/kindsundev/expense/manager/ui/home/HomeActivity.kt
+++ app/src/main/java/com/kindsundev/expense/manager/ui/home/HomeActivity.kt
@@ -5,10 +5,7 @@ import android.os.Bundle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.kindsundev.expense.manager.R
-import com.kindsundev.expense.manager.common.Logger
-import com.kindsundev.expense.manager.data.firebase.UserFirebase
 import com.kindsundev.expense.manager.databinding.ActivityHomeBinding
-import kotlin.system.exitProcess
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -17,10 +14,9 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setupBottomNav()
-        val user = UserFirebase().user
-        Logger.warn(user?.displayName.toString())
-        Logger.warn(user?.email.toString())
+        if (savedInstanceState == null) {
+            setupBottomNav()
+        }
     }
 
     private fun setupBottomNav() {
