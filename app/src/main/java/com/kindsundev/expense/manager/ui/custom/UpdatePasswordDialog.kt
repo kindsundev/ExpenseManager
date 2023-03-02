a4309157219d58c3ae21ccea56c8525241142ab0
kind sun dev <mr.duongtiendev@gmail.com>
2023-03-03 04:19:13 +0700

    init update name dialog, update email dialog, update password dialog

---
+++ app/src/main/java/com/kindsundev/expense/manager/ui/custom/UpdatePasswordDialog.kt
@@ -0,0 +1,58 @@
+package com.kindsundev.expense.manager.ui.custom
+
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.kindsundev.expense.manager.R
+import com.kindsundev.expense.manager.databinding.DialogUpdatePasswordBinding
+
+class UpdatePasswordDialog : DialogFragment() {
+    private var _binding: DialogUpdatePasswordBinding? = null
+    private val binding get() = _binding
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        _binding = DialogUpdatePasswordBinding.inflate(layoutInflater)
+
+        val dialog = MaterialAlertDialogBuilder(
+            requireActivity(), R.style.Theme_ExpenseManager).apply {
+            setView(binding!!.root)
+        }.create()
+
+        dialog.window?.apply {
+            setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+            setGravity(Gravity.CENTER)
+            setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        }
+
+        return dialog
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        initListener()
+        return binding!!.root
+    }
+
+    private fun initListener() {
+        binding!!.btnBack.setOnClickListener { dialog!!.dismiss() }
+        binding!!.btnUpdate.setOnClickListener {
+            dialog!!.dismiss()
+            // ...
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
