75b144ee3d35bbfc696942106819d431b5ed1f3b
kind sun dev <mr.duongtiendev@gmail.com>
2023-03-04 09:59:22 +0700

    check valid name when update user name

--- app/src/main/java/com/kindsundev/expense/manager/common/Constant.kt
+++ app/src/main/java/com/kindsundev/expense/manager/common/Constant.kt
@@ -4,5 +4,9 @@ class Constant {
     companion object {
         const val LOADING_DIALOG_NAME = "LOADING_FRAGMENT"
         const val LOGOUT_DIALOG_NAME = "LOGOUT_FRAGMENT"
+        const val UPDATE_NAME_DIALOG_NAME = "UPDATE_NAME_FRAGMENT"
+        const val UPDATE_EMAIL_DIALOG_NAME = "UPDATE_EMAIL_FRAGMENT"
+        const val UPDATE_PASSWORD_DIALOG_NAME = "UPDATE_PASSWORD_FRAGMENT"
+        const val SPECIAL_CHARACTERS = "[!@#$%&*()_+=|<>?{}\\[\\]~-]"
     }
 }
--- app/src/main/java/com/kindsundev/expense/manager/utils/DataUtils.kt
+++ app/src/main/java/com/kindsundev/expense/manager/utils/DataUtils.kt
@@ -1,7 +1,9 @@
 package com.kindsundev.expense.manager.utils
 
 import android.util.Patterns
+import com.kindsundev.expense.manager.common.Constant
 import com.kindsundev.expense.manager.common.Status
+import java.util.regex.Pattern
 
 fun checkEmailAndPassword(email: String, password: String): Status {
     return if (email.isEmpty()) {
@@ -17,4 +19,31 @@ fun checkEmailAndPassword(email: String, password: String): Status {
     } else {
         Status.VALID_DATA
     }
+}
+
+fun checkName(name: String): Status {
+    return if (name.isEmpty()) {
+        Status.WRONG_NAME_EMPTY
+    } else if (isAShortName(name)) {
+        Status.WRONG_NAME_SHORT
+    } else if (isALongName(name)) {
+        Status.WRONG_NAME_LONG
+    } else if (isNumeric(name)) {
+        Status.WRONG_NAME_HAS_DIGITS
+    } else if (isSpecialCharacter(name)) {
+        Status.WRONG_NAME_HAS_SPECIAL_CHARACTER
+    } else {
+        Status.VALID_DATA
+    }
+}
+
+private fun isAShortName(name: String) = name.length < 3
+
+private fun isALongName(name: String) = name.length > 70
+
+private fun isNumeric(toCheck: String) = toCheck.all { character -> character.isDigit() }
+
+private fun isSpecialCharacter(toCheck: String): Boolean {
+    val matcher = Pattern.compile(Constant.SPECIAL_CHARACTERS).matcher(toCheck)
+    return matcher.find()
 }
