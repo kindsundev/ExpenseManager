4a1a6bce1c1a74a403716c94d808998770b7c830
kind sun dev <mr.duongtiendev@gmail.com>
2023-03-08 16:32:28 +0700

    remove AppGlideModule, because it is deprecated

--- app/src/main/java/com/kindsundev/expense/manager/ui/custom/CustomGlideModule.kt
+++
@@ -1,7 +0,0 @@
-package com.kindsundev.expense.manager.ui.custom
-
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.module.AppGlideModule
-
-@GlideModule
-class CustomGlideModule : AppGlideModule() {}
