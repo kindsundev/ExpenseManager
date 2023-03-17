e7476ca45dd323738d54704a534d71b826843cb7
kind sun dev <mr.duongtiendev@gmail.com>
2023-03-17 16:36:49 +0700

    merge on click listener to wallet contract

---
+++ app/src/main/java/com/kindsundev/expense/manager/ui/home/note/transaction/bottomsheet/WalletContract.kt
@@ -0,0 +1,20 @@
+package com.kindsundev.expense.manager.ui.home.note.transaction.bottomsheet
+
+import com.kindsundev.expense.manager.data.model.WalletModel
+import com.kindsundev.expense.manager.ui.base.BaseView
+
+interface WalletContract {
+
+    interface Presenter {
+        fun handlerCreateWallet(id: Int, name: String, currency: String, balance: String)
+    }
+
+    interface View : BaseView {
+        fun onSuccess(message: String)
+    }
+
+    interface Listener {
+        fun onClickWalletItem(wallet: WalletModel)
+    }
+
+}
--- app/src/main/java/com/kindsundev/expense/manager/ui/home/note/transaction/bottomsheet/WalletListener.kt
+++
@@ -1,9 +0,0 @@
-package com.kindsundev.expense.manager.ui.home.note.transaction.bottomsheet
-
-import com.kindsundev.expense.manager.data.model.WalletModel
-
-interface WalletListener {
-
-    fun onClickWalletItem(wallet: WalletModel)
-
-}
