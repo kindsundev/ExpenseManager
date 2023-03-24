cb4678fe4ee9be37276d50c8d137faca6b27ef98
kind sun dev <mr.duongtiendev@gmail.com>
2023-03-24 21:54:24 +0700

    handle the balance display event

--- app/src/main/java/com/kindsundev/expense/manager/ui/home/wallet/BagFragment.kt
+++ app/src/main/java/com/kindsundev/expense/manager/ui/home/wallet/BagFragment.kt
@@ -6,13 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.kindsundev.expense.manager.R
+import com.kindsundev.expense.manager.common.Constant
+import com.kindsundev.expense.manager.data.shared.PreferenceHelper
 import com.kindsundev.expense.manager.databinding.FragmentBagBinding
 import com.kindsundev.expense.manager.ui.home.wallet.exchange.ExchangeAdapter
+import com.kindsundev.expense.manager.utils.onFeatureIsDevelop
 import com.kindsundev.expense.manager.utils.showToast
 
 class BagFragment : Fragment(), BagContract.Listener {
     private var _binding : FragmentBagBinding? = null
     private val binding get() = _binding
+
+    private var stateBalanceVisibility: Boolean = true
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +27,7 @@ class BagFragment : Fragment(), BagContract.Listener {
     ): View {
         _binding = FragmentBagBinding.inflate(inflater, container, false)
         initRecyclerViewTransactions()
+        initListener()
         return binding!!.root
     }
 
@@ -41,9 +48,29 @@ class BagFragment : Fragment(), BagContract.Listener {
         return mList
     }
 
+    private fun initListener() {
+        binding!!.btnUpgradePremium.setOnClickListener { activity?.onFeatureIsDevelop() }
+        binding!!.btnVisibility.setOnClickListener { onClickVisibilityBalance() }
+        binding!!.btnSearch.setOnClickListener {  }
+        binding!!.btnNotifications.setOnClickListener {  }
+    }
+
+    private fun onClickVisibilityBalance() {
+        if (stateBalanceVisibility) {
+            binding!!.btnVisibility.setImageResource(R.drawable.ic_visibility)
+            binding!!.itemBalance.visibility = View.GONE
+            stateBalanceVisibility = false
+        } else {
+            binding!!.btnVisibility.setImageResource(R.drawable.ic_visibility_off)
+            binding!!.itemBalance.visibility = View.VISIBLE
+            stateBalanceVisibility = true
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        stateBalanceVisibility = false
     }
 
     override fun onClickTransaction(transaction: String) {
