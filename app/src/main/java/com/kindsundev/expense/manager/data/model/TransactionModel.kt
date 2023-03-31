09499b2bfccfe279be01f06559c7333a6fa73b65
kind sun dev <mr.duongtiendev@gmail.com>
2023-03-31 08:50:23 +0700

    change Serialization interface

--- app/src/main/java/com/kindsundev/expense/manager/data/model/TransactionModel.kt
+++ app/src/main/java/com/kindsundev/expense/manager/data/model/TransactionModel.kt
@@ -1,8 +1,7 @@
 package com.kindsundev.expense.manager.data.model
 
-import android.os.Parcel
-import android.os.Parcelable
 import com.kindsundev.expense.manager.common.Constant
+import java.io.Serializable
 
 data class TransactionModel(
     val id: Int? = 0,
@@ -11,39 +10,4 @@ data class TransactionModel(
     val amount: Double? = 0.0,
     val date: String? = "",
     val note: String? = Constant.FAKE_DATA_NULL
-) : Parcelable {
-
-    constructor(parcel: Parcel) : this(
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readValue(Double::class.java.classLoader) as? Double,
-        parcel.readString(),
-        parcel.readString()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeValue(id)
-        parcel.writeString(type)
-        parcel.writeString(category)
-        parcel.writeValue(amount)
-        parcel.writeString(date)
-        parcel.writeString(note)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<TransactionModel> {
-        override fun createFromParcel(parcel: Parcel): TransactionModel {
-            return TransactionModel(parcel)
-        }
-
-        override fun newArray(size: Int): Array<TransactionModel?> {
-            return arrayOfNulls(size)
-        }
-    }
-
-}
+) : Serializable
--- app/src/main/java/com/kindsundev/expense/manager/data/model/WalletModel.kt
+++ app/src/main/java/com/kindsundev/expense/manager/data/model/WalletModel.kt
@@ -1,8 +1,9 @@
 package com.kindsundev.expense.manager.data.model
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class WalletModel(
     val id: Int? = 0,
     val name: String? = "",
@@ -10,39 +11,4 @@ data class WalletModel(
     val origin: Double? = 0.0,
     val balance: Double? = 0.0,
     val transaction: TransactionModel? = null
-) : Parcelable {
-
-    constructor(parcel: Parcel) : this(
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readValue(Double::class.java.classLoader) as? Double,
-        parcel.readValue(Double::class.java.classLoader) as? Double,
-        parcel.readParcelable(TransactionModel::class.java.classLoader)
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeValue(id)
-        parcel.writeString(name)
-        parcel.writeString(currency)
-        parcel.writeValue(origin)
-        parcel.writeValue(balance)
-        parcel.writeParcelable(transaction, flags)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<WalletModel> {
-        override fun createFromParcel(parcel: Parcel): WalletModel {
-            return WalletModel(parcel)
-        }
-
-        override fun newArray(size: Int): Array<WalletModel?> {
-            return arrayOfNulls(size)
-        }
-    }
-
-}
+) : Parcelable
