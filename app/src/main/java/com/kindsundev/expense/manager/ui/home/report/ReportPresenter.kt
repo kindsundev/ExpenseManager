e8fe02d6a4af4c69bcff5f83c2859995074cc500
kind sun dev <mr.duongtiendev@gmail.com>
2023-04-22 15:11:36 +0700

    move fun report to utils class

--- app/src/main/java/com/kindsundev/expense/manager/ui/home/report/ReportPresenter.kt
+++ app/src/main/java/com/kindsundev/expense/manager/ui/home/report/ReportPresenter.kt
@@ -1,20 +1,19 @@
 package com.kindsundev.expense.manager.ui.home.report
 
 import com.github.mikephil.charting.data.Entry
-import com.kindsundev.expense.manager.data.model.BillModel
-import com.kindsundev.expense.manager.data.model.TransactionModel
 import com.kindsundev.expense.manager.data.model.WalletModel
-import java.text.SimpleDateFormat
-import java.util.*
+import com.kindsundev.expense.manager.utils.BalanceReportUtils
 import kotlin.collections.ArrayList
 
 class ReportPresenter(
     private val view: ReportContract.View
 ): ReportContract.Presenter {
+    private val balanceReport = BalanceReportUtils()
 
     fun getBalanceHistorySevenDays(wallet: WalletModel): ArrayList<Entry> {
         val mBalanceInLastSevenDays = ArrayList<Entry>()
-        val balanceHistory = wallet.calculateBalanceEveryDayForLastSevenDays()
+        val bills = wallet.getBills()
+        val balanceHistory = balanceReport.calculateBalanceEveryDayForLastSevenDays(wallet.balance!!, bills)
         if (balanceHistory.isNotEmpty()) {
             balanceHistory.forEachIndexed { index, value ->
                 mBalanceInLastSevenDays.add(Entry(index.toFloat(), value.toFloat()))
---
+++ app/src/main/java/com/kindsundev/expense/manager/utils/BalanceReportUtils.kt
@@ -0,0 +1,56 @@
+package com.kindsundev.expense.manager.utils
+
+import com.kindsundev.expense.manager.data.model.BillModel
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
+class BalanceReportUtils {
+
+    fun calculateBalanceEveryDayForLastSevenDays(
+        currentBalance: Double,
+        bills: ArrayList<BillModel>
+    ): ArrayList<Double> {
+        val balanceInLastSevenDays = ArrayList<Double>()
+        val amountInLastSevenDays = calculateAmountEveryDayForLastSevenDays(bills)
+        currentBalance.let {
+            var result = currentBalance
+            amountInLastSevenDays.forEach { amount ->
+                if (amount > 0) result -= amount else result += (-amount)
+                balanceInLastSevenDays.add(result)
+            }
+            if (balanceInLastSevenDays.isNotEmpty()) {
+                balanceInLastSevenDays.removeLast()         // change currentBalance
+                balanceInLastSevenDays.reverse()            // before sort to get last seven days
+                balanceInLastSevenDays.add(currentBalance)
+            }
+        }
+//        balanceInLastSevenDays.forEach { Logger.info(String.format("%.0f", it)) }
+        return balanceInLastSevenDays
+    }
+
+    private fun calculateAmountEveryDayForLastSevenDays(bills: ArrayList<BillModel>): ArrayList<Double> {
+        val amountInLastSevenDays = ArrayList<Double>()
+        val listOfLastSevenDays = getListOfLastSevenDays(bills)
+        if (listOfLastSevenDays.isNotEmpty()) {
+            listOfLastSevenDays.forEach { bill ->
+                val result = bill.calculateBalance()
+                amountInLastSevenDays.add(result)
+//                Logger.error(result.toString())
+            }
+        }
+        return amountInLastSevenDays
+    }
+
+    private fun getListOfLastSevenDays(bills: ArrayList<BillModel>): List<BillModel> {
+        var lastSevenDays : List<BillModel> = listOf()
+        if (bills.isNotEmpty()) {
+            lastSevenDays = bills.sortedByDescending { bill ->
+                bill.date?.let {
+                    SimpleDateFormat("dd E MMMM yyyy", Locale.ENGLISH).parse(it)
+                }
+            }.take(7)
+        }
+        return lastSevenDays
+    }
+}
