1984e7207c5158abf7c86680d2a4c466cc9f8c84
kind sun dev <mr.duongtiendev@gmail.com>
2023-04-24 17:12:49 +0700

    delete fun is never using

--- app/src/main/java/com/kindsundev/expense/manager/data/firebase/TransactionFirebase.kt
+++ app/src/main/java/com/kindsundev/expense/manager/data/firebase/TransactionFirebase.kt
@@ -8,7 +8,6 @@ import com.kindsundev.expense.manager.utils.getCurrentDate
 import com.kindsundev.expense.manager.data.model.BillModel
 import io.reactivex.Completable
 import io.reactivex.Observable
-import io.reactivex.ObservableEmitter
 
 class TransactionFirebase : BaseFirebase() {
 
@@ -42,46 +41,6 @@ class TransactionFirebase : BaseFirebase() {
                     }
                 }
             }
-    }
-
-    fun getTransactions(walletID: String): Observable<BillModel> =
-        Observable.create { subscriber ->
-            initPointerGeneric()
-                .child(walletID)
-                .child(Constant.MY_REFERENCE_CHILD_TRANSACTION)
-                .addValueEventListener(object : ValueEventListener {
-                    override fun onDataChange(snapshot: DataSnapshot) {
-                        handlerResultWhenGetTransactionOnSuccess(subscriber, snapshot)
-                    }
-
-                    override fun onCancelled(error: DatabaseError) {
-                        subscriber.onError(error.toException())
-                    }
-                })
-        }
-
-    private fun handlerResultWhenGetTransactionOnSuccess(
-        subscriber: ObservableEmitter<BillModel>,
-        snapshot: DataSnapshot
-    ) {
-        if ((!subscriber.isDisposed) && (snapshot.hasChildren())) {
-            for (data in snapshot.children) {
-                val transactions = ArrayList<TransactionModel>()
-                val date  = data.key
-                for (child in data.children) {
-                    val dataChild = child.getValue(TransactionModel::class.java)
-                    dataChild?.let { transactions.add(it) }
-                }
-                val bill = BillModel(date, transactions)
-                subscriber.onNext(bill)
-            }
-            subscriber.onComplete()
-        } else {
-            //  subscriber.onError(NullPointerException())
-            val transaction = BillModel("Null", ArrayList())
-            subscriber.onNext(transaction)
-            subscriber.onComplete()
-        }
     }
 
     fun getTransactionsInDay(walletID: String, date: String) : Observable<BillModel> =
