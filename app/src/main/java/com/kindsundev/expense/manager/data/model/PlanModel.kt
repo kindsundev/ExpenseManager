330ee3d17ec193ec1cc357d2d787c057fd4a90f1
kind sun dev <mr.duongtiendev@gmail.com>
2023-05-01 18:04:11 +0700

    create plan model

---
+++ app/src/main/java/com/kindsundev/expense/manager/data/model/PlanModel.kt
@@ -0,0 +1,15 @@
+package com.kindsundev.expense.manager.data.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class PlanModel(
+    val id: Int? = 0,
+    val name: String? = "",
+    val estimatedAmount: Double? = 0.0,
+    val startDate: String? = "",
+    val endDate: String? = "",
+    val isWorking: Boolean? = true,
+    val isSuccess: Boolean? = false
+) : Parcelable
