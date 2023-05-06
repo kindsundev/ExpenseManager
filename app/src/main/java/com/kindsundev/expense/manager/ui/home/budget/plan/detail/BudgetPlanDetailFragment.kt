3e164e7844b62fd818f9365625f90fe3cc8a5525
kind sun dev <mr.duongtiendev@gmail.com>
2023-05-06 17:02:58 +0700

    init current data of plan to detailed plan fragment

--- app/src/main/java/com/kindsundev/expense/manager/ui/home/budget/plan/detail/BudgetPlanDetailFragment.kt
+++ app/src/main/java/com/kindsundev/expense/manager/ui/home/budget/plan/detail/BudgetPlanDetailFragment.kt
@@ -6,14 +6,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
 import com.kindsundev.expense.manager.R
+import com.kindsundev.expense.manager.data.model.PlanModel
 import com.kindsundev.expense.manager.databinding.FragmentBudgetPlanDetailBinding
+import com.kindsundev.expense.manager.utils.formatDisplayCurrencyBalance
 import com.kindsundev.expense.manager.utils.showMessage
+import kotlin.properties.Delegates
 
 class BudgetPlanDetailFragment : Fragment() {
     private var _binding: FragmentBudgetPlanDetailBinding? = null
     private val binding get() = _binding
     private var isLayoutInfoVisible = true
+
+    private val args by navArgs<BudgetPlanDetailFragmentArgs>()
+    private var mWalletId by Delegates.notNull<Int>()
+    private lateinit var mPlan: PlanModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,8 +30,22 @@ class BudgetPlanDetailFragment : Fragment() {
     ): View {
         _binding = FragmentBudgetPlanDetailBinding.inflate(layoutInflater)
         binding!!.toolbar.inflateMenu(R.menu.plan_detail_menu)
+        mPlan = args.plan
+        mWalletId = args.walletId
+        initPlanData()
         initListener()
         return binding!!.root
+    }
+
+    private fun initPlanData() {
+        binding!!.tvName.text = mPlan.name
+        binding!!.tvCurrentBalance.text = formatDisplayCurrencyBalance(mPlan.currentBalance.toString())
+        binding!!.tvEstimatedAmount.text = formatDisplayCurrencyBalance(mPlan.estimatedAmount.toString())
+
+        val startDay = mPlan.startDate!!.split(", ")
+        val endDay = mPlan.endDate!!.split(", ")
+        binding!!.tvStartDate.text = startDay[1]
+        binding!!.tvEndDate.text = endDay[1]
     }
 
     private fun initListener() {
